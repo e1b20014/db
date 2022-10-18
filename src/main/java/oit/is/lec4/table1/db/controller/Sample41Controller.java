8bbe44c20e13acc04161e625b3d617efef77dc7f
e1b20062T <e1b20062@oit.ac.jp>
2022-10-18 15:51:36 +0900

    入室したことをDBに記録(Insert)する処理を実装する

--- src/main/java/oit/is/lec4/table1/db/controller/Sample41Controller.java
+++ src/main/java/oit/is/lec4/table1/db/controller/Sample41Controller.java
@@ -1,11 +1,16 @@
 package oit.is.lec4.table1.db.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.ModelMap;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.lec4.table1.db.model.Chamber;
 import oit.is.lec4.table1.db.model.ChamberMapper;
@@ -33,6 +38,19 @@ public class Sample41Controller {
     model.addAttribute("chamber2", chamber2);
 
     return "sample41.html";
+  }
+
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    Chamber chamber3 = new Chamber();
+    chamber3.setChamberName(chamberName);
+    chamber3.setUserName(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    // System.out.println("ID:" + chamber3.getId());
+    return "sample43.html";
   }
 
 
--- src/main/java/oit/is/lec4/table1/db/model/ChamberMapper.java
+++ src/main/java/oit/is/lec4/table1/db/model/ChamberMapper.java
@@ -2,10 +2,17 @@ package oit.is.lec4.table1.db.model;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface ChamberMapper {
 
   @Select("SELECT id,userName,chamberName from chamber where id = #{id}")
   Chamber selectById(int id);
+
+  @Insert("INSERT INTO chamber (userName,chamberName) VALUES (#{userName},#{chamberName});")
+  @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+  void insertChamber(Chamber chamber);
+
 }
