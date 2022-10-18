34042f109ae2769611d4e0276436ee7c5997fac9
e1b20014 <e1b20014@oit.ac.jp>
2022-10-18 14:06:24 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/lec4/table1/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec4.table1.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
