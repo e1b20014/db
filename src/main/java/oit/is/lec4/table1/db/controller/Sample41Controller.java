287aff1fc6c2c185a4d6558142c69ab2e9fd9f35
take0x <89313929+take0x@users.noreply.github.com>
2022-10-18 15:20:12 +0900

    DBの先頭のデータをSELECTで一つだけ取得して表示する

--- src/main/java/oit/is/lec4/table1/db/controller/Sample41Controller.java
+++ src/main/java/oit/is/lec4/table1/db/controller/Sample41Controller.java
@@ -1,15 +1,32 @@
 package oit.is.lec4.table1.db.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.ModelMap;
+
+import oit.is.lec4.table1.db.model.Chamber;
+import oit.is.lec4.table1.db.model.ChamberMapper;
 
 @Controller
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
   @GetMapping("step1")
   public String sample41() {
+    return "sample41.html";
+  }
+
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
     return "sample41.html";
   }
 }
