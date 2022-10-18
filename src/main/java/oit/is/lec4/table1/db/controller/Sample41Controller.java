204d7c1293de75e3f26c15166df72f197cbb3a74
take0x <89313929+take0x@users.noreply.github.com>
2022-10-18 14:59:32 +0900

    sample41.htmlを表示するGETリクエストを実装する

---
+++ src/main/java/oit/is/lec4/table1/db/controller/Sample41Controller.java
@@ -0,0 +1,15 @@
+package oit.is.lec4.table1.db.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample4")
+public class Sample41Controller {
+
+  @GetMapping("step1")
+  public String sample41() {
+    return "sample41.html";
+  }
+}
